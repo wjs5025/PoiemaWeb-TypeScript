b9d642d55154a493a7f6f552c35ce40c83d74479
저니녁 <wjs_5025@naver.com>
2022-12-21 17:35:14 +0900

    add : 실습 파일 추가

---
+++ training/Start.js
@@ -0,0 +1,8 @@
+"use strict";
+class Start {
+  static main() {
+    console.log("helloworld");
+    return 0;
+  }
+}
+Start.main();
---
+++ training/Start.ts
@@ -0,0 +1,8 @@
+class Start {
+  public static main(): number {
+    console.log("helloworld");
+    return 0;
+  }
+}
+
+Start.main();
