6201d689c6e5eaac9576b67c35e54d0be0784435
wjs5025 <wjs_5025@naver.com>
2022-12-22 20:58:04 +0900

    feat : 클래스 관련 내용 추가

--- training/playground.ts
+++ training/playground.ts
@@ -15,3 +15,29 @@ let value = 4; // value는 number
 // 타입캐스팅
 const $input = document.querySelector('input["type="text"]');
 // $input : Element | null
+
+// 클래스
+class Student {
+  constructor(name: string) {
+    console.log(name);
+  }
+
+  static staticMethod() {
+    return "Method";
+  }
+
+  prototypeMethod() {
+    return "Proto";
+  }
+}
+
+const student = new Student("Jeon");
+// console.log(student.name); // error : student 에는 name 프로퍼티가 없음.
+
+// 정적 메소드
+
+console.log(Student.staticMethod());
+
+const student2 = new Student("Jeon");
+// console.log(student2.staticMethod()); // error : 정적메소드이므로 인스턴스로 호출할 수 없다.
+// console.log(Student.prototypeMethod()); // error
